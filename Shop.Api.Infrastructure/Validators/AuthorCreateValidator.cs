f1ab358952cd327820a31ac669c3ee3778f02a50
Marlody Rodriguez <marlodycairo@hotmail.com>
2022-06-07 00:01:27 -0500

    Actualization AuthorCreateValidation

--- Shop.Api.Infrastructure/Validators/AuthorCreateValidator.cs
+++ Shop.Api.Infrastructure/Validators/AuthorCreateValidator.cs
@@ -14,11 +14,15 @@ namespace PruebaTecnicaCrud.Api.Infrastructure.Validators
         {
             RuleFor(x => x.FullName)
                 .Cascade(CascadeMode.Stop)
-                .NotEmpty().Length(0, 250).WithMessage("Ingrese el nombre completo");
+                .NotNull().WithMessage("No se aceptan valores nulos. Debe ingresar el nombre completo");
 
             RuleFor(x => x.FullName)
                 .Cascade(CascadeMode.Stop)
-                .NotNull().Length(0, 250).WithMessage("No se aceptan valores nulos. Debe ingresar el nombre completo");
+                .NotEmpty().WithMessage("Ingrese el nombre completo");
+
+            RuleFor(x => x.FullName)
+                .Cascade(CascadeMode.Stop)
+                .Length(0, 250).WithMessage("el nombre excede el tamaño permitido.");
 
             RuleFor(x => x.BirthDate)
                 .Cascade(CascadeMode.Stop)
@@ -28,9 +32,22 @@ namespace PruebaTecnicaCrud.Api.Infrastructure.Validators
                 .Cascade(CascadeMode.Stop)
                 .NotEmpty().WithMessage("Debe ingresar un correo");
 
+            RuleFor(x => x.Email)
+                .Cascade(CascadeMode.Stop)
+                .EmailAddress(FluentValidation.Validators.EmailValidationMode.AspNetCoreCompatible)
+                .WithMessage("Ingrese una dirección de correo válida.");
+
+            RuleFor(x => x.Email)
+                .Cascade(CascadeMode.Stop)
+                .Length(0, 250).WithMessage("El correo excede el tamaño permitido");
+
             RuleFor(x => x.CityOrigin)
                 .Cascade(CascadeMode.Stop)
-                .NotEmpty();
+                .NotEmpty().WithMessage("Ingrese la ciudad");
+
+            RuleFor(x => x.CityOrigin)
+                .Cascade(CascadeMode.Stop)
+                .Length(0, 150).WithMessage("El nombre de la ciudad excede el tamaño permitido");
         }
     }
 }
